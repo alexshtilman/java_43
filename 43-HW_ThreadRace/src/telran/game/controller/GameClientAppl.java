7e622559ca70ad208a8743f7be2f2b74dd911eaf
Alex Shtilman <gungam@outlook.com>
2020-11-21 23:22:03 +0200

    update

--- 43-HW_ThreadRace/src/telran/game/controller/GameClientAppl.java
+++ 43-HW_ThreadRace/src/telran/game/controller/GameClientAppl.java
@@ -2,6 +2,7 @@ package telran.game.controller;
 
 import java.time.Instant;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -9,7 +10,20 @@ import java.util.stream.Stream;
 import client.telran.view.*;
 import telran.game.service.GameService;
 
+class Sortbyplace implements Comparator<GameService> {
+	// Used for sorting in ascending order of
+	// roll name
+	public int compare(GameService a, GameService b) {
+		if(a.finishPlace - b.finishPlace == 0) {
+			return (int) (a.finishTime - b.finishTime) ;
+		}
+		return a.finishPlace - b.finishPlace;
+	}
+}
+
 public class GameClientAppl {
+
+	private static final int LINES = 50;
 
 	public static void main(String[] args) {
 		InputOutput io = new ConsoleInputOutput();
@@ -18,18 +32,17 @@ public class GameClientAppl {
 	}
 
 	public static void startRace(InputOutput io) {
-		
-		int numOfThreads = io.readInteger("number of threads",3,10000);
-		int distance = io.readInteger("number of threads",100,10000);	
+
+		int numOfThreads = io.readInteger("number of threads", 3, 10000);
+		int distance = io.readInteger("number of threads", 100, 10000);
 		var start = Instant.now();
-		
-		List<GameService> rides = Stream.generate(()->new GameService(distance,start))
-				.limit(numOfThreads)
+
+		List<GameService> rides = Stream.generate(() -> new GameService(distance, start)).limit(numOfThreads)
 				.collect(Collectors.toList());
-		
+
 		rides.forEach(GameService::start);
-		
-		rides.forEach(r->{
+
+		rides.forEach(r -> {
 			try {
 				r.join();
 			} catch (InterruptedException e) {
@@ -39,12 +52,20 @@ public class GameClientAppl {
 
 		System.out.println("Collections.synchronizedList");
 		GameService.getFromCollection();
-		
+
 		System.out.println("ReentrantLock");
 		GameService.getLocked();
-		
+
 		System.out.println("ReentrantLock");
 		GameService.getSync();
-		
+
+		System.out.println("AtomicInteger");
+		System.out.println("=".repeat(LINES));
+
+		Collections.sort(rides, new Sortbyplace());
+
+		for (GameService ride : rides) {
+			System.out.println(ride.getResults());
+		}
 	}
 }
--- 43-HW_ThreadRace/src/telran/game/service/GameService.java
+++ 43-HW_ThreadRace/src/telran/game/service/GameService.java
@@ -6,16 +6,21 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class GameService extends Thread {
+	private static final int LINES = 50;
 	int distance;
 	Instant start;
 	protected static List<String> lockedParticipants = new ArrayList<>();
 	protected static List<String> syncParticipants = new ArrayList<>();
-
-	public static List<String> winners = Collections.synchronizedList(new ArrayList<String>());
+	static AtomicInteger curretPlace = new AtomicInteger(0)	 ;
+	public long finishTime;
+	public int finishPlace;
+	String threadNumber;
+	public static ConcurrentLinkedDeque winners = new ConcurrentLinkedDeque( new ArrayList<>());
 	ReentrantLock lock = new ReentrantLock();
 
 	public GameService(int distance, Instant start) {
@@ -24,15 +29,49 @@ public class GameService extends Thread {
 		lockedParticipants.clear();
 		syncParticipants.clear();
 		winners.clear();
+		finishTime = 0;
+		finishPlace = 0;
+		curretPlace.getAndSet(0);
 	}
 
 	private static void printList(List<String> list) {
 		int i = 1;
-		System.out.println("=".repeat(80));
+		System.out.println("=".repeat(LINES));
 		for (String p : list) {
-			System.out.printf("place-%d: %s\n", i++, p);
+			System.out.printf("%s at %d place\n", p, i++);
 		}
-		System.out.println("=".repeat(80));
+		System.out.println("=".repeat(LINES));
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + distance;
+		result = prime * result + finishPlace;
+		result = prime * result + (int) (finishTime ^ (finishTime >>> 32));
+		result = prime * result + ((lock == null) ? 0 : lock.hashCode());
+		result = prime * result + ((start == null) ? 0 : start.hashCode());
+		result = prime * result + ((threadNumber == null) ? 0 : threadNumber.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		GameService other = (GameService) obj;
+		if (distance != other.distance)
+			return false;
+		if (finishPlace != other.finishPlace)
+			return false;
+		if (finishTime != other.finishTime)
+			return false;
+		return true;
 	}
 
 	public static void getSync() {
@@ -44,12 +83,19 @@ public class GameService extends Thread {
 	}
 
 	public static void getFromCollection() {
-		printList(winners);
+		
+		
+		System.out.println("=".repeat(LINES));
+		
+		for (int i=1;i<winners.size()+2;i++) {
+			System.out.printf("%s at %d place\n", winners.pollLast(), i);
+		}
+		System.out.println("=".repeat(LINES));
 	}
 
 	@Override
 	public void run() {
-		String threadNumber = Thread.currentThread().getName();
+		this.threadNumber = Thread.currentThread().getName();
 		for (int i = 0; i < distance; i++) {
 			System.out.println(threadNumber);
 			try {
@@ -59,23 +105,30 @@ public class GameService extends Thread {
 			}
 		}
 		long time = ChronoUnit.MILLIS.between(start, Instant.now());
-
-		winners.add(threadNumber + ":" + time);
+		finishPlace = curretPlace.addAndGet(1);
+		finishTime = time;
+		
+		winners.addLast(threadNumber + " finished with time " + time);
 
 		lock.lock();
 		try {
-			lockedParticipants.add(threadNumber + ":" + time);
+			lockedParticipants.add(threadNumber + " finished with time " + time);
 		} finally {
 			lock.unlock();
 		}
 
 		synchronized (GameService.class) {
-			syncParticipants.add(threadNumber + ":" + time);
+			syncParticipants.add(threadNumber + " finished with time " + time);
 		}
 
 	}
+	
 
+	
 	public int getRandomInt(int min, int max) {
 		return new Random().nextInt(max - min) + min;
+	}
+	public String getResults() {
+		return String.format("%s finished with time %s at %d place",threadNumber,finishTime,finishPlace);
 	}
 }
